d42f3f94e32cc5612ae08b06c9debe478b331ba0
virolialo <virolialo@gmail.com>
2025-04-29 17:07:26 +0200

    conf: conexión a la base de datos de neo4j #1

--- idealista/idealista/settings.py
+++ idealista/idealista/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webapp',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración de Neo4j
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "idealista11"
