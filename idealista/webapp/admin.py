feb81eab7fc8d15a537ace539b9055f4c4052b05
virolialo <virolialo@gmail.com>
2025-05-31 17:53:10 +0200

    feat: creacion del modelo de hiperparametros #14

--- idealista/webapp/admin.py
+++ idealista/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Vivienda, Barriada, Hiperparametros
 
-# Register your models here.
+admin.site.register(Vivienda)
+admin.site.register(Barriada)
+admin.site.register(Hiperparametros)
