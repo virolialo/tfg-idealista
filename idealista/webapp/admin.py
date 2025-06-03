f95983c194366e786d40c9e82c1bd7b5b8e1ee88
virolialo <virolialo@gmail.com>
2025-06-03 17:32:43 +0200

    feat: vista admin de los modelos #14

--- idealista/webapp/admin.py
+++ idealista/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Vivienda, Barriada, Hiperparametros
+from .models import Vivienda, Barriada, Hiperparametros, Metro
 
 admin.site.register(Vivienda)
 admin.site.register(Barriada)
 admin.site.register(Hiperparametros)
+admin.site.register(Metro)
